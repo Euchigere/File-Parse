d2a72ed57af8df65a06b1e70806cb87875515bd2
euchigere <ugochigere02@gmail.com>
2020-08-04 22:15:44 +0100

    modified

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,23 @@
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 import java.io.File;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String mode = "production";
-		if (args.length > 0) {
+		String mode = "production"; 	// default mode
+
+		if (args.length > 0) {          // assign mode if supplied from the command line argument
 			mode = args[0].toLowerCase();
 		}
 
 		ConfigParser config;
 
+		// create a new configParser object depending on mode variable
 		if ("production".equals(mode)) {
 				config = new ConfigParser(new File("src/config.txt"));
 		} else  {
 			File configFile = new File("src/config-" + mode + ".txt");
+			// verify if config file for the mode exists in relative path
 			if (!configFile.exists()) {
 				System.err.println("File does not exist");
 				return;
@@ -26,51 +25,10 @@ public class Main {
 			config = new ConfigParser(configFile);
 		}
 
+		// Print all the entries in the config file
 		System.out.printf("%-25s VALUE%n-----------------------------------%n", "KEY");
-		for (Map.Entry<String, String> entry : config.getConfigKeyValuePair().entrySet()) {
+		for (Map.Entry<String, String> entry : config.getConfigEntryMap().entrySet()) {
 			System.out.printf("%-25s %s%n", entry.getKey(), entry.getValue());
 		}
-	}
-}
-
-class ConfigParser {
-	Map<String, String> configKeyValuePair;
-
-	public ConfigParser(File configFilePath) {
-		configKeyValuePair = new HashMap<>();
-		parseFile(configFilePath);
-	}
-
-	private void parseFile(File configFilePath) {
-		try (Scanner in = new Scanner(configFilePath)) {
-			String prefix = "";
-			while (in.hasNext()) {
-				String lineText = in.nextLine();
-				if (lineText.isBlank()) {
-					prefix = "";
-				} else if (!lineText.contains("=")) {
-					prefix = lineText.substring(1, lineText.length() - 1) + ".";
-				} else {
-					String[] keyValuePair = lineText.split("=");
-					if (configKeyValuePair.containsKey(prefix + keyValuePair[0])) {
-						continue;
-					}
-					configKeyValuePair.put(
-							prefix + keyValuePair[0],
-							keyValuePair[1]
-					);
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public String get(String key) {
-		return configKeyValuePair.get(key);
-	}
-
-	public Map<String, String> getConfigKeyValuePair() {
-		return (HashMap) ((HashMap) configKeyValuePair).clone();
 	}
 }
